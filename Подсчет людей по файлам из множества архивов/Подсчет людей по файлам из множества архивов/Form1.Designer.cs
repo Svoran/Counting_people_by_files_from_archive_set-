85cfdf6cc5f1efc6f1539f5c1b7532569fff9d24
Svoran <55567337+Svoran@users.noreply.github.com>
2022-11-04 20:35:20 +1100

    Add files via upload

---
+++ Подсчет людей по файлам из множества архивов/Подсчет людей по файлам из множества архивов/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Подсчет_людей_по_файлам_из_множества_архивов
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.BTN_Open_and_Count = new System.Windows.Forms.Button();
+			this.Result_rtb = new System.Windows.Forms.RichTextBox();
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.SuspendLayout();
+			// 
+			// BTN_Open_and_Count
+			// 
+			this.BTN_Open_and_Count.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.BTN_Open_and_Count.Location = new System.Drawing.Point(14, 15);
+			this.BTN_Open_and_Count.Name = "BTN_Open_and_Count";
+			this.BTN_Open_and_Count.Size = new System.Drawing.Size(145, 61);
+			this.BTN_Open_and_Count.TabIndex = 0;
+			this.BTN_Open_and_Count.Text = "Выбрать архивы";
+			this.BTN_Open_and_Count.UseVisualStyleBackColor = true;
+			this.BTN_Open_and_Count.Click += new System.EventHandler(this.BTN_Open_and_Count_Click);
+			// 
+			// Result_rtb
+			// 
+			this.Result_rtb.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.Result_rtb.Location = new System.Drawing.Point(171, 17);
+			this.Result_rtb.Name = "Result_rtb";
+			this.Result_rtb.Size = new System.Drawing.Size(619, 421);
+			this.Result_rtb.TabIndex = 1;
+			this.Result_rtb.Text = "";
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.FileName = "openFileDialog1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.Result_rtb);
+			this.Controls.Add(this.BTN_Open_and_Count);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button BTN_Open_and_Count;
+		private System.Windows.Forms.RichTextBox Result_rtb;
+		private System.Windows.Forms.OpenFileDialog openFileDialog;
+	}
+}
+
---
+++ Подсчет людей по файлам из множества архивов/Подсчет людей по файлам из множества архивов/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Aspose.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Подсчет_людей_по_файлам_из_множества_архивов
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Open_and_Count_Click(object sender, EventArgs e)//Метод включает в себя извлечение данных из необходимых архивов.
+        {
+            string[] ArchiveFilesPath;
+            string ArchiveFileName;
+            string fileDirectory;
+            int All_counter=0;
+
+            //Установка свойств объекта openFileDialog для выбора нескольких архивов в папке с датами
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "C:\\";
+            openFileDialog.Filter = "Archive files (*.rar)|*.rar|Archive files (*.zip)|*.zip";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.Multiselect = true;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {             
+                ArchiveFilesPath = openFileDialog.FileNames;//Список всех выбранных архивов
+                FileInfo fileInfo = new FileInfo(ArchiveFilesPath[0]);
+                fileDirectory = fileInfo.DirectoryName;//Путь к выбранным архивам
+                fileInfo = new FileInfo(fileDirectory);
+                string Data = fileInfo.Name;
+                Result_rtb.Text="Количество людей за " + Data + ":\n";
+
+                for (int i=0; i < ArchiveFilesPath.Length; i++)//Цикл обработки каждого архива
+                {
+                    ArchiveFileName = openFileDialog.SafeFileNames[i].Remove(openFileDialog.SafeFileNames[i].Length - 4);//Имя архива для определения принадлежности количества к архиву
+                    string SpisokName = fileDirectory + "\\Spisok_" + ArchiveFileName + "_.txt";//Имя для создаваемого промежуточного обрабатываемого файла
+                    using (Archive archive = new Archive(ArchiveFilesPath[i]))//Использование сторонней библиотеки Aspose.Zip, позволяющей извлекать данные файлов из архивов
+                    {
+                        // Создайте файл с помощью метода Create().
+                        using (var destination = File.Create(SpisokName))//Создается промежуточный файл. Способ взят из официальных примеров использования библиотеки, в сущности, можно было бы обрабатывать сразу поток байт.
+                        {
+                            // Откройте запись из архива RAR. Файл выбирается в зависимости от порядка записи на архив (???).
+                            using (var source = archive.Entries[40].Open())
+                            {
+                                byte[] buffer = new byte[1024];
+                                int bytesRead;
+                                // Запись извлеченных данных в файл.
+                                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                                 destination.Write(buffer, 0, bytesRead);
+                            }
+                        }
+                    }
+                    All_counter+=Spisok_Handler(SpisokName, ArchiveFileName);//Подсчет общего числа людей
+                    File.Delete(SpisokName);//Удаление промежуточных файлов
+                }
+                Result_rtb.AppendText("\nОбщее число людей: " + All_counter);
+            }
+        }
+
+        private int Spisok_Handler(string SpisokName, string ArchiveFileName)//Обрабатывает файл с входящим именем для подсчета людей-строк
+        {
+            int counter=0;
+            foreach (string line in System.IO.File.ReadLines(SpisokName))//Для каждой строки в промежуточном файле счетчик увеличивается на 1
+            {
+                counter++;
+            }
+            Result_rtb.AppendText(ArchiveFileName + ": " + counter + "\n");
+            return counter;
+        }
+    }
+}
